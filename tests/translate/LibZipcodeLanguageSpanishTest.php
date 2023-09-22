7888a5e167da6a53adf599804dc203dea2187db1
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:50:13 -0300

    correção tradução es-py

--- tests/translate/LibZipcodeLanguageSpanishTest.php
+++ tests/translate/LibZipcodeLanguageSpanishTest.php
@@ -19,7 +19,7 @@ class LibZipcodeLanguageSpanishTest extends TestCase
         
         $path = base_path() . '/vendor/codificar/zipcode/src/resources/lang';
         $directoryPt = "$path/pt-br/*.php";
-        $directoryEs = "$path/es/*.php";
+        $directoryEs = "$path/es-py/*.php";
         $filesPt = glob($directoryPt);
         $filesEs = glob($directoryEs);
 
