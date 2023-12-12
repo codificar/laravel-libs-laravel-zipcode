03450ea4ddecc34bd4551da9365d6ba6048ae4b5
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2023-12-12 11:35:01 -0300

    corrigindo arquivo de tradução en-gb

---
+++ src/resources/lang/en-gb/zipcode.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'zip_code'                  => 'ZIP Code',
+    'zip_code_settings'         => 'ZIP Code Settings',
+    'zip_code_provider'         => 'ZIP Code Provider',
+    'zip_code_key'              => 'ZIP Code Key',
+    'zip_code_red_enable'       => 'Enable Redundancy',
+    'zip_code_red_provider'     => 'Redundancy ZIP Code Provider',
+    'zip_code_red_key'          => 'Redundancy ZIP Code Key',
+    'generic_save'              => 'Save',
+    'generic_yes'               => 'Yes',
+    'generic_no'                => 'No',
+    'generic_required'          => 'Fill in this field'
+);
